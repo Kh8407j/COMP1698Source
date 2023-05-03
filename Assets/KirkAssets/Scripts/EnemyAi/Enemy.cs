f3b06645db27edef3afeeb29ae29432eb00755b7
Kh8407j <kh8407j@gre.ac.uk>
2023-05-03 04:32:26 +0100

    Beginning progress on enemies

---
+++ Assets/KirkAssets/Scripts/EnemyAi/Enemy.cs
@@ -0,0 +1,22 @@
+// KHOGDEN 001115381
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace platformer
+{
+    public class Enemy : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
--- Assets/KirkAssets/Scripts/Scroll.cs
+++ Assets/KirkAssets/Scripts/Scroll.cs
@@ -3,29 +3,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Scroll : MonoBehaviour
+namespace platformer
 {
-    [Header("Scroll Settings")]
-    [SerializeField] float scrollSpeedRate = 1f;
-    [SerializeField] float resetXPosAt = -10f;
-
-    // KH - Holds reference of the transform's position during awake.
-    private Vector2 awakePos;
-
-    // KH - Called before 'void Start()'.
-    private void Awake()
+    public class Scroll : MonoBehaviour
     {
-        awakePos = transform.position;
-    }
+        [Header("Scroll Settings")]
+        [SerializeField] float scrollSpeedRate = 1f;
+        [SerializeField] float resetXPosAt = -10f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        // KH-  Continuously make the level scroll to the left to give the illusion everything's moving.
-        transform.Translate(new Vector3(-scrollSpeedRate * Time.deltaTime, 0f, 0f));
+        // KH - Holds reference of the transform's position during awake.
+        private Vector2 awakePos;
 
-        // KH - To give the illusion the level is endless, reposition the transform when it scrolls too far out.
-        if (transform.position.x < resetXPosAt)
-            transform.position = awakePos;
+        // KH - Called before 'void Start()'.
+        private void Awake()
+        {
+            awakePos = transform.position;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // KH-  Continuously make the level scroll to the left to give the illusion everything's moving.
+            transform.Translate(new Vector3(-scrollSpeedRate * Time.deltaTime, 0f, 0f));
+
+            // KH - To give the illusion the level is endless, reposition the transform when it scrolls too far out.
+            if (transform.position.x < resetXPosAt)
+                transform.position = awakePos;
+        }
     }
 }
